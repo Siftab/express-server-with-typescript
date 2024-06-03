70b98f52204910f7732ac4baffaef97a57362565
Siftab <siiffuuu@gmail.com>
2024-06-04 00:31:05 +0600

    parser added

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express')
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+// using parser
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send('typescript Server');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(500).send("hitted post Route");
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,19 @@
 // const express = require('express')
-import express from 'express'
+import express, { Request, Response } from 'express'
 const app = express()
 
 
-app.get('/', (req, res) => {
+// using parser
+app.use(express.json())
+
+
+app.get('/', (req: Request, res: Response) => {
     res.send('typescript Server')
+})
+
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body)
+    res.status(500).send("hitted post Route")
 })
 
 export default app;
